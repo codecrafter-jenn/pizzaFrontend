1e6449ca54c16c9d78a8bf249b7293cd90c7cccc
Jenn <jenniferdgaylord@gmail.com>
2024-07-25 11:53:45 -0500

    grid layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,11 +7,15 @@
     <div class="container">
     <img src="../assets/ivan-torres-MQUqbmszGGM-unsplash (4).jpg">
     <div class="center">
+      <div class="grid">
         <img class="logo" src="../assets/logo.png">
         <h1>BLAZE PIZZA & GOURMET</h1>
         <h3>Crafted with Passion, Fired to Perfection</h3>
+      </div>
+      <div class="grid2">
         <p>We're passionate about creating delicious, mouth-watering pizzas that bring joy to your taste buds. Our commitment to quality and freshness shines through in every bite, whether you're enjoying The Works, a Meat Lover's, or a custom creation topped with your favorite ingredients.</p>
         <button>See the menu &#8680</button>
+      </div>
     </div>
     <img src="../assets/pizza.png" class="pizza">
     </div>
@@ -46,7 +50,6 @@ img {
   display: grid;
 }
 .center {
-  position: relative;
   width: 50%;
   height: 80%;
   z-index: 2;
@@ -56,55 +59,73 @@ img {
   margin-left: 10%;
   margin-top: 10%;
   background-image: linear-gradient(to right, #989872, rgba(255,0,0,0));
+  position: relative;
+}
+.grid {
+  position:absolute;
+  top: 2%;
+  left: 2%;
+  width: 70%;
+  height: 30%;
+  display: grid;
+  grid-template-columns: 1fr 2fr ;
+  grid-template-areas: 
+    "logo header"
+    "logo tagline"   
+}
+.grid2 {
+  position:absolute;
+  top: 32%;
+  left: 2%;
+  width: 50%;
+  height: 66%;
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  grid-template-areas: 
+    "main main main "
+    ". button ."
+    ;
 }
 .logo {
   border: none;
-  margin: 2%;
-  width: 20%;
-  position: absolute;
+  grid-area: logo;
+  align-self: center;
 }
 h1 {
-  width: 65%;
-  height: 10%;
-  top: 5%;
   text-wrap: nowrap;
   color: antiquewhite;
-  display: inline-block;
   font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
   font-weight: bold;
-  position: absolute;
-  left: 25%;
+  grid-area: header;
+  align-self: flex-end;
+  font-size: 2vw;
 }
 button {
   background-color:#661A1D;
   color: antiquewhite;
-  position: absolute;
-  top: 70%;
-  left: 12%;
-  width: 30%;
-  height: 10%;
+  height: 40%;
   border-radius: 5px;
   box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.3);
   font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-  font-size:x-large;
+  font-size: 1.8vw;
+  grid-area: button;
 }
 h3{
-  position: absolute;
-  left: 25%;
-  top: 12%;
   font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
   font-weight: 300;
   border-bottom: #661A1D 3px solid;
+  grid-area: tagline;
+  align-self: flex-start;
+  text-wrap: nowrap;
+  font-size: 1vw;
 }
 p {
-  position: absolute;
   color: antiquewhite;
-  top: 25%;
-  right: 50%;
-  margin-left: 5%;
   font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-  font-size: x-large;
+  font-size: 1.5vw;
   text-align: center;
+  grid-area: main;
+  align-self: center;
 }
 
 </style>
